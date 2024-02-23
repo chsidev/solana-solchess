5e781a4dddd9ef87bc9ae9690d1486a728db6fdf
chsidev <127224337+chsidev@users.noreply.github.com>
2024-02-23 12:25:49 +0700

    Cleanup old print statements

--- src/client/util/game-data-layout.js
+++ src/client/util/game-data-layout.js
@@ -3,3 +3,4 @@ import * as BufferLayout from "buffer-layout";
 export const gameAccountDataLayout = BufferLayout.struct([
   BufferLayout.seq(BufferLayout.u8(), 138, "game_arr"),
 ]);
+# Change 1 on 2024-02-23
